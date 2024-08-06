45ff97fd826375f1d920d7466069e305b89701c2
Kwon9302 <148173045+Kwon9302@users.noreply.github.com>
2024-08-06 23:33:19 +0900

    first commit

---
+++ src/main/java/com/cos/sequrity1/Sequrity1Application.java
@@ -0,0 +1,13 @@
+package com.cos.sequrity1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sequrity1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sequrity1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/cos/sequrity1/Sequrity1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.sequrity1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sequrity1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
