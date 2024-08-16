71c04f63c643c266dc0a685cf99cee5293a7a735
Kwon9302 <148173045+Kwon9302@users.noreply.github.com>
2024-08-16 13:18:05 +0900

    240816 카카오 로그인 구현 중...

--- src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
@@ -2,6 +2,7 @@ package com.cos.security1.config.oauth;
 
 import com.cos.security1.config.auth.PrincipalDetails;
 import com.cos.security1.config.oauth.provider.GoogleUserInfo;
+import com.cos.security1.config.oauth.provider.KakaoUserInfo;
 import com.cos.security1.config.oauth.provider.NaverUserInfo;
 import com.cos.security1.config.oauth.provider.OAuth2UserInfo;
 import com.cos.security1.model.User;
@@ -29,9 +30,10 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 
         // code를 통해 구성한 정보
         System.out.println("userRequest clientRegistration : " + userRequest.getClientRegistration());
+        System.out.println("userRequest AccessToken : "+ userRequest.getAccessToken().getTokenValue());
         // token을 통해 응답받은 회원정보
         System.out.println("oAuth2User : " + oAuth2User);
-
+        System.out.println("oAuth2User.getAttributes() : " + oAuth2User.getAttributes());
         return processOAuth2User(userRequest, oAuth2User);
     }
         // OAuth2 요청으로 들어온 정보에따라 정보 가공
@@ -40,16 +42,18 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
         // Attribute를 파싱해서 공통 객체로 묶는다. 관리가 편함.
         OAuth2UserInfo oAuth2UserInfo = null;
         if (userRequest.getClientRegistration().getRegistrationId().equals("google")) {
-            System.out.println("구글 로그인 요청~~");
-                                               // oAuth2User는 Map방식이다.
+            System.out.println("구글 로그인 요청~~"); // oAuth2User는 Map방식이다.
             oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes()); // 의존성 주입
-            System.out.println("oAuth2UserInfo : " + oAuth2UserInfo);
 
         } else if (userRequest.getClientRegistration().getRegistrationId().equals("naver")){
             System.out.println("네이버 로그인 요청~~");
             oAuth2UserInfo = new NaverUserInfo(oAuth2User.getAttributes()); // 의존성 주입
-            System.out.println("oAuth2UserInfo : " + oAuth2UserInfo);
-        } else {
+
+        } else if (userRequest.getClientRegistration().getRegistrationId().equals("kakao")){
+            System.out.println("카카오 로그인 요청~~");
+            oAuth2UserInfo = new KakaoUserInfo(oAuth2User.getAttributes()); // 의존성 주입
+
+        }else {
             System.out.println("우리는 구글과 네이버만 지원해요 ㅎㅎ");
         }
 
@@ -57,22 +61,22 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
         System.out.println("oAuth2UserInfo.getProviderId() : " + oAuth2UserInfo.getProviderId());
 
         // userOptional을 통해 naver, google로그인에 성공한 user들을 db에 저장한다.
-        Optional<User> userOptional =
-                userRepository.findByProviderAndProviderId(oAuth2UserInfo.getProvider(), oAuth2UserInfo.getProviderId());
+//        Optional<User> userOptional =
+//                userRepository.findByProviderAndProviderId(oAuth2UserInfo.getProvider(), oAuth2UserInfo.getProviderId());
+                 User userEmail = userRepository.findByEmail(oAuth2UserInfo.getEmail());
+        // DB에 해당 email이 없을 경우 가입 불가.
 
-        User user;
-        if (userOptional.isPresent()) {
-            System.out.println("DB에 정보가 있음 ==> user update실시");
-            user = userOptional.get();
-            // user가 존재하면 update 해주기
-            user.setEmail(oAuth2UserInfo.getEmail());
-            userRepository.save(user);
+
+        User user = null;
+        if (userEmail == null) {
+            System.out.println("가입 권한이 없습니다");
         } else {
-            // user의 패스워드가 null이기 때문에 OAuth 유저는 일반적인 로그인을 할 수 없음.
+            //            // user의 패스워드가 null이기 때문에 OAuth 유저는 일반적인 로그인을 할 수 없음.
             System.out.println("user not found ==> oAuthUser정보 db 저장");
+
             user = User.builder()
                     // provider + providerId를 조합해서 name으로 저장
-                    .username(oAuth2UserInfo.getProvider() + "_" + oAuth2UserInfo.getProviderId())
+                    .username(oAuth2UserInfo.getName())
                     .email(oAuth2UserInfo.getEmail())
                     .role("ROLE_USER") // 가입시 기본 role 부여
                     .provider(oAuth2UserInfo.getProvider())
@@ -80,6 +84,28 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
                     .build();
             userRepository.save(user);
         }
+
+        // User user;
+//        if (userOptional.isPresent()) {
+//            System.out.println("DB에 정보가 있음 ==> user update실시");
+//            user = userOptional.get();
+//            // user가 존재하면 update 해주기
+//            user.setEmail(oAuth2UserInfo.getEmail());
+//            userRepository.save(user);
+//        } else {
+//            // user의 패스워드가 null이기 때문에 OAuth 유저는 일반적인 로그인을 할 수 없음.
+//            System.out.println("user not found ==> oAuthUser정보 db 저장");
+//
+//            user = User.builder()
+//                    // provider + providerId를 조합해서 name으로 저장
+//                    .username(oAuth2UserInfo.getProvider() + "_" + oAuth2UserInfo.getProviderId())
+//                    .email(oAuth2UserInfo.getEmail())
+//                    .role("ROLE_USER") // 가입시 기본 role 부여
+//                    .provider(oAuth2UserInfo.getProvider())
+//                    .providerId(oAuth2UserInfo.getProviderId())
+//                    .build();
+//            userRepository.save(user);
+//        }
         return new PrincipalDetails(user, oAuth2User.getAttributes());
     }
 }
---
+++ src/main/java/com/cos/security1/config/oauth/provider/KakaoUserInfo.java
@@ -0,0 +1,38 @@
+package com.cos.security1.config.oauth.provider;
+
+
+import lombok.AllArgsConstructor;
+
+import java.util.Map;
+
+@AllArgsConstructor
+public class KakaoUserInfo implements OAuth2UserInfo {
+
+    private Map<String, Object> attributes;
+
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public String getProviderId() {
+        return attributes.get("id").toString();
+    }
+
+    @Override
+    public String getName() {
+        return (String) ((Map) attributes.get("properties")).get("nickname");
+    }
+
+    @Override
+    public String getProvider() {
+        return "kakao";
+    }
+
+    @Override
+    public String getEmail() {
+        return (String) ((Map) attributes.get("kakao_account")).get("email");
+    }
+
+
+}
